f66832b8d61216fa758932387b411b1db9889a0b
DFYX <dfyx@wecallit42.de>
2025-02-25 20:48:09 +0100

    Make file type priorities more flexible

--- Tools/AutoStack.cs
+++ Tools/AutoStack.cs
@@ -53,7 +53,7 @@ internal class AutoStack
 
             if (copyMetadata)
             {
-                var rawImageAsset = sortedAssets.LastOrDefault(a => GetFileTypePriority(a) == 0);
+                var rawImageAsset = sortedAssets.LastOrDefault(a => GetFileTypePriority(a) == -1);
                 if (rawImageAsset != null)
                 {
                     foreach (var asset in sortedAssets.Where(a => a.LocalDateTime != rawImageAsset.LocalDateTime))
@@ -91,43 +91,45 @@ internal class AutoStack
     }
 
     // List taken from https://github.com/immich-app/immich/blob/main/server/src/utils/mime-types.ts
-    private static readonly HashSet<string> RawExtensions = [
-        ".3fr",
-        ".ari",
-        ".arw",
-        ".cap",
-        ".cin",
-        ".cr2",
-        ".cr3",
-        ".crw",
-        ".dcr",
-        ".dng",
-        ".erf",
-        ".fff",
-        ".iiq",
-        ".k25",
-        ".kdc",
-        ".mrw",
-        ".nef",
-        ".nrw",
-        ".orf",
-        ".ori",
-        ".pef",
-        ".psd",
-        ".raf",
-        ".raw",
-        ".rw2",
-        ".rwl",
-        ".sr2",
-        ".srf",
-        ".srw",
-        ".x3f",
-    ];
+    private static readonly Dictionary<string, int> FileTypePriorities = new() {
+        { ".3fr", -1 },
+        { ".ari", -1 },
+        { ".arw", -1 },
+        { ".cap", -1 },
+        { ".cin", -1 },
+        { ".cr2", -1 },
+        { ".cr3", -1 },
+        { ".crw", -1 },
+        { ".dcr", -1 },
+        { ".dng", -1 },
+        { ".erf", -1 },
+        { ".fff", -1 },
+        { ".iiq", -1 },
+        { ".k25", -1 },
+        { ".kdc", -1 },
+        { ".mrw", -1 },
+        { ".nef", -1 },
+        { ".nrw", -1 },
+        { ".orf", -1 },
+        { ".ori", -1 },
+        { ".pef", -1 },
+        { ".raf", -1 },
+        { ".raw", -1 },
+        { ".rw2", -1 },
+        { ".rwl", -1 },
+        { ".sr2", -1 },
+        { ".srf", -1 },
+        { ".srw", -1 },
+        { ".x3f", -1 },
+
+        // Consider .psd edited
+        { ".psd", 1 }
+    };
 
     private static int GetFileTypePriority(Asset asset)
     {
         var extension = Path.GetExtension(asset.OriginalFileName).ToLowerInvariant();
-        return RawExtensions.Contains(extension) ? 0 : 1;
+        return FileTypePriorities.TryGetValue(extension, out var priority) ? priority : 0;
     }
 
     private static readonly Regex BaseNameRegex = new Regex("\\A(?<BaseName>[a-zA-Z]+_[0-9]+)([_-].*)?\\Z");
